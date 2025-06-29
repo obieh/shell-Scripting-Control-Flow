94349a4ca021eff1a6201e6bf9507ce0ece87078
obieh <o_amadic@yahoo.com>
2025-06-29 23:47:14 +0100

    added elif block

--- control_flow.sh
+++ control_flow.sh
@@ -9,5 +9,7 @@ read -p "Enter a number: " num
 echo "You have entered the number $num"
 if [ $num -gt 0 ]; then
     echo "The number is positive."
+elif [ $num -lt 0 ]; then
+    echo "The number is negative"
 fi
 
