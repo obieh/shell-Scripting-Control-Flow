c3112342ac1f55dc373c31ed623a53c3687eae73
obieh <o_amadic@yahoo.com>
2025-06-30 15:35:33 +0100

    for loop

---
+++ for_loop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Simple for loop to print numbers 1 through 5
+
+echo "Counting from 1 to 5:"
+
+# Loop from 1 to 5
+for i in 1 2 3 4 5  # 'i' takes each value in this list
+do
+    echo "Number: $i"  # Print the current number
+done
