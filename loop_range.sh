aff7422ebe8a3ad407af0c180ee38830ce69d679
obieh <o_amadic@yahoo.com>
2025-07-01 02:09:03 +0100

    add range script

---
+++ loop_range.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Author: ObieShenk
+# Date: Date: $(date +%Y-%m-%d)
+# Simple Number Looping Script
+# This script demonstrates a basic for loop that:
+# 1. Counts from 1 to 5
+# 2. Prints a message for each number
+
+echo "-----------------------"
+
+# The main for loop
+for i in {1..5}  # This generates numbers from 1 to 5
+do
+    # Print the message with the current number
+    echo "Processing number $i"
+    
+done
+
+echo "-----------------------"
+
